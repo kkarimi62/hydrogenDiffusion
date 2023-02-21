7ab310d3f7bf331d77a3ed2d6f0257e0b20e0944
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-02-21 16:30:57 +0100

    updates

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 					10:' -var ParseData 1 -var DataFile swapped_600.dat',
 					'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
 					'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
-					'p2':' %s 3.52 10.0 10.0 10.0 data_minimized.txt 5 1 1.0'%(os.getcwd()+'/lmpScripts'),
+					'p2':' %s 3.52 10.0 10.0 10.0 data_minimized.txt 5 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
 					'p3':' data_minimized.txt init_xyz.conf %s 1400.0'%(os.getcwd()+'/lmpScripts'),
 					'p4':' data_minimized.txt data_minimized.txt %s 1'%(os.getcwd()+'/lmpScripts'),
 					'p5':' ',
