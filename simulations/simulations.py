8f26aacfc727473b63cd96aa734de8c28d4e211b
kkarimi62 <kamran.karimi1362@gmail.com>
2023-03-13 11:29:54 +0100

    updates

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
 	#        print('EXEC=',EXEC)
 		#
 		EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
-		durtn = ['95:59:59','00:59:59','167:59:59'][ 2 ]
+		durtn = ['95:59:59','00:14:59','167:59:59'][ 1 ]
 		mem = '22gb'
 		partition = ['gpu-v100','parallel','cpu2019','single'][2]
 		#--
