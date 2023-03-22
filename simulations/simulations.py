aae58b3e742b9d23ca3a1cfd88df6dba49df710e
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-03-23 00:19:18 +0100

    updates

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
 		nruns	 = range(1)
 		#
-		nThreads = 4 #8
+		nThreads = 8
 		nNode	 = 1
 		#
 		jobname  = {
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 					5:'hydrogenDiffusionInAlT1000KDislocated', 
 					6:'test', 
 					4:'mitStuff2nd', 
-				   }[6]
+				   }[5]
 		sourcePath = os.getcwd() +\
 					{	
 						0:'/junk',
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 					6:' -var buff 0.0 -var T 300 -var P 0.0 -var gammaxy 1.0 -var gammadot 1.0e-04 -var nthermo 10000 -var ndump 1000 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
 					4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
 					5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var natoms 1000 -var ntype 2 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
-					51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_init.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
+					51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_atom_added.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
 					7:' -var buff 0.0 -var T 1500.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
 					71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
 					72:' -var buff 0.0 -var T 1500.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
@@ -125,7 +125,7 @@ if __name__ == '__main__':
 					4:[5, 'p6',51,'p3','p5',1.0], #--- create lattice, add H, minimize, kart input, kart.sh to bash shell ,invoke kart
 					5:['p2', 'p6',51,'p3','p5',1.0], #--- put a dislocation, add H, minimize, kart input, kart.sh to bash shell ,invoke kart
 					6:['p2',51,'p3','p5',2.0], #--- put a dislocation, minimize, kart input, kart.sh to bash shell ,invoke kart
-				  }[6]
+				  }[5]
 		Pipeline = list(map(lambda x:LmpScript[x],indices))
 	#	Variables = list(map(lambda x:Variable[x], indices))
 		EXEC = list(map(lambda x:np.array(['lmp','py','kmc'])[[ type(x) == type(0), type(x) == type(''), type(x) == type(1.0) ]][0], indices))	
