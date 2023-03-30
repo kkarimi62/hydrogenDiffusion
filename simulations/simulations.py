7e739c82c6f3948c295e628df7a16d89f4ea0caa
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-03-30 09:04:42 +0200

    updates

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
 		nruns	 = range(1)
 		#
-		nThreads = 8
+		nThreads = 4
 		nNode	 = 1
 		#
 		jobname  = {
@@ -140,7 +140,7 @@ if __name__ == '__main__':
 	#        print('EXEC=',EXEC)
 		#
 		EXEC_lmp = ['lmp_mpi','lmp_serial','_lmp'][0]
-		durtn = ['95:59:59','00:09:59','167:59:59'][ 1 ]
+		durtn = ['95:59:59','00:29:59','167:59:59'][ 1 ]
 		mem = '8gb'
 		partition = ['gpu-v100','parallel','cpu2019','single'][1]
 		#--
