1a7c7a853eb04e5cc03eb849c4f28e9d2df070c9
kkarimi62 <kamran.karimi1362@gmail.com>
2023-03-31 09:15:17 +0200

    updates

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -3,21 +3,17 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-	lnums = [ 36, 115  ]
+	lnums = [ 36, 116  ]
 	string=open('simulations.py').readlines() #--- python script
 #	lnums = [ 33, 97   ]
 #	string=open('simulations-ncbj.py').readlines() #--- python script
 	#---
-#	n=6
-#	Temps  = dict(zip(range(2,n),range(2,n)))
-	l = 26.0
 	Temps=			{
-				0:2*l,
-				1:3*l,
-				2:4*l,
-				3:5*l,
-				4:6*l,
-				5:7*l,
+				0:1200,
+				1:1400,
+				2:1600,
+				3:1800,
+				4:2000,
 			}
 	#---
 	count = 0
@@ -25,10 +21,11 @@ if __name__ == '__main__':
 				temp = Temps[keys_t]
 			#---	densities
 				inums = lnums[ 0 ] - 1
-				string[ inums ] = "\t6:\'hydrogenDiffusionInAlMultipleLength/length%s\',\n"%(keys_t) #--- change job name
+				string[ inums ] = "\t6:\'hydrogenDiffusionInAlMultipleTemps/temp%s\',\n"%(keys_t) #--- change job name
 			#---
 				inums = lnums[ 1 ] - 1
-				string[ inums ] = "\t\'p21\':\' %%s 3.52 %s 18.0 26.0 data_init.txt 2 2 1.0 0.0\'%%(os.getcwd()+\'/lmpScripts\'),\n"%temp
+				string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf %%s %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%temp
+#				string[ inums ] = "\t\'p21\':\' %%s 3.52 %s 18.0 26.0 data_init.txt 2 2 1.0 0.0\'%%(os.getcwd()+\'/lmpScripts\'),\n"%temp
 #				string[ inums ] = "\t\'p7\':\' sortieproc.0 %s Topo_ignore\',\n"%(temp)
 				
 #				string[ inums ] = "\t72:\' -var buff 0.0 -var T %s -var nevery 10 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_%s.dat\',\n"%(temp,temp)
