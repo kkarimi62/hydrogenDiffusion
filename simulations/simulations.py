d4d05fb92ef100333114cffeaa7514d78719a709
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-05-16 16:05:01 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     #
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
-                5:'hydrogenOnly',#'pureCrystall', 
+                5:'hydrogenOnlyMultiple',#'pureCrystall', 
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                 4:'mitStuff2nd', 
                }[5]
@@ -117,7 +117,7 @@ if __name__ == '__main__':
                 'p3':' data_minimized.txt init_xyz.conf %s 1000.0'%(os.getcwd()+'/lmpScripts'),
                 'p4':' data_minimized.txt data_minimized.txt %s 1'%(os.getcwd()+'/lmpScripts'),
                 'p5':' ',
-                'p6':' %s data_minimized.txt data_minimized.txt 1'%(os.getcwd()+'/lmpScripts'),
+                'p6':' %s data_minimized.txt data_minimized.txt 10'%(os.getcwd()+'/lmpScripts'),
                 'p7':' sortieproc.0 0 Topo_ignore',
                  1.0:'-x DataFile=data_minimized.txt',
                  2.0:'-x DataFile=data_minimized.txt',
