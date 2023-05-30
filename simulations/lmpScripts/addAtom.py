4c88cb6b56e1c7cf30c2f7955af7b5f44746db8e
kamran.karimi1 <kkarimi62>
2023-05-30 01:34:02 -0600

    updates

--- simulations/lmpScripts/addAtom.py
+++ simulations/lmpScripts/addAtom.py
@@ -27,7 +27,8 @@ def WriteDataFile(AtomskOutpt,outpt):
     distance = 0.0 #3.0 #--- distance from extended dislocation
     
     rcent = np.matmul(box.CellVector,np.array([.5,.5,.5])) + box.CellOrigin
-    rcent += np.array([-0.5*l,distance,-0.5*l]) #--- new origin
+    rcent += np.array([-0.5*l,-0.5*l,-0.5*l]) #--- new origin
+#    rcent += np.array([-0.5*l,distance,-0.5*l]) #--- new origin
     
     loo=rcent
     hii=rcent+np.matmul(np.array([[l,0,0],[0,l,0],[0,0,l]]),np.array([1,1,1]))
@@ -37,15 +38,44 @@ def WriteDataFile(AtomskOutpt,outpt):
     #--- wrap
     wrap = lp.Wrap( atoms, box )
     filtr = wrap.isInside()
+
+    sfile=open('ovito.xyz','w')
+    utl.PrintOvito(pd.DataFrame(df[filtr]), sfile, 'junk', attr_list=['type','x', 'y', 'z','id'])
+    sfile.close()
+
+    
+
 #    pdb.set_trace()
-    AL_ids = df[filtr].index
+    AL_ids = df[filtr].id #index
+    print('n=',len(AL_ids))
     for ID in AL_ids:
         os.system('atomsk %s -add-atom H near %s final.cfg'%(inputt,ID))
         os.system('atomsk final.cfg lmp')
         os.system('mv final.lmp %s'%inputt)
         os.system('rm *.cfg')
-    if inputt != outpt:    
-        os.system('mv %s %s'%(inputt,outpt))
+
+    #--- perturb and write
+    lmpData = lp.ReadDumpFile( inputt )
+    lmpData.ReadData()
+    #--- atom obj
+    df = lmpData.coord_atoms_broken[0]
+    xyz_perturbed = np.c_[df['x y z'.split()]] + np.random.normal(loc=0.0, scale=0.1, size=3*len(df)).reshape((len(df),3))
+    df_new = pd.DataFrame(np.c_[df['id type'.split()],xyz_perturbed],columns='id type x y z'.split())
+
+    atoms = lp.Atoms( **df_new.to_dict(orient='series') )
+    #--- box
+    box = lp.Box( BoxBounds = lmpData.BoxBounds[0],AddMissing = np.array([0.0,0.0,0.0] ) )
+    mass=lmpData.mass #{1:26.98153860,2:1.00800000}
+#    pdb.set_trace()
+    wd = lp.WriteDataFile(atoms, box,mass)
+    wd.Write(outpt) #,itime=itime,
+                 #attrs=['id', 'type', 'x', 'y', 'z','val'], 
+                 #fmt='%i %i %4.3e %4.3e %4.3e %4.3e')
+
+#    if inputt != outpt:    
+#        os.system('mv %s %s'%(inputt,outpt))
+
+    
     #--- hydrogen atoms outside
 #    isHyd = df['type'] == 2
 #    hydOutside = np.all([~filtr,isHyd],axis=0)
@@ -68,6 +98,7 @@ natom = int(sys.argv[4])
 
 sys.path.append(lib_path)
 import LammpsPostProcess2nd as lp
+import utility as utl 
 
 #os.system('atomsk %s -add-atom H random %s final.cfg'%(inputt,natom))
 #os.system('atomsk final.cfg lmp')
