e3df6b10ab3f1d7f04df4b830a49bb2f57087799
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-09 21:01:52 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
-                5:'pureHydrogenDiffusion_md', #'pureHydrogenDiffusion5th', 
+                5:'pureHydrogenDiffusion_md_MultipleTemps/temp0', #'pureHydrogenDiffusion5th', 
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                }[5]
     sourcePath = os.getcwd() +\
--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -3,22 +3,22 @@ if __name__ == '__main__':
     import os
     import numpy as np
     #---
-#    lnums = [ 36, 116  ]
-#    string=open('simulations.py').readlines() #--- python script
-    lnums = [ 39, 116, 121 ]
-    string=open('simulations-ncbj-slurm.py').readlines() #--- python script
+    lnums = [ 36, 72  ]
+    string=open('simulations.py').readlines() #--- python script
+#    lnums = [ 39, 116, 121 ]
+#    string=open('simulations-ncbj-slurm.py').readlines() #--- python script
     #---
     Temps=			{
-#                 0:1200,
-#                 1:1400,
-#                 2:1600,
-#                 3:1800,
-#                 4:2000,
-                0:600,
-                1:700,
-                2:800,
-                3:900,
-                4:1000,
+                0:1000,
+                1:1200,
+                2:1400,
+                3:1600,
+                4:1800,
+#                 0:600,
+#                 1:700,
+#                 2:800,
+#                 3:900,
+#                 4:1000,
             }
     #---
     count = 0
@@ -26,8 +26,8 @@ if __name__ == '__main__':
                 temp = Temps[keys_t]
             #---	densities
                 inums = lnums[ 0 ] - 1
-#                string[ inums ] = "\t6:\'hydrogenDiffusionInAlBigMultipleTemps10H/temp%s\',\n"%(keys_t) #--- change job name
-                string[ inums ] = "\t7:\'biCrystalMultipleTemp/temp%s\',\n"%(keys_t) #--- change job name
+                string[ inums ] = "\t5:\'pureHydrogenDiffusion_md_MultipleTemps/temp%s\',\n"%(keys_t) #--- change job name
+#                string[ inums ] = "\t7:\'biCrystalMultipleTemp/temp%s\',\n"%(keys_t) #--- change job name
             #---
                 inums = lnums[ 1 ] - 1
 #                string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf %%s %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%temp
@@ -36,8 +36,8 @@ if __name__ == '__main__':
 
                 string[ inums ] = "\t72:\' -var seed %%s -var buff 0.0 -var buffy 5.0 -var T %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat\'%%np.random.randint(1001,9999),\n"%(temp)
 
-                inums = lnums[ 2 ] - 1
-                string[ inums ] = "\t12:\' -var buff 0.0 -var buffy 5.0 -var T %s -var swap_every 100 -var swap_atoms 267 -var rn %%s -var dump_every 100 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile traj.dump\'%%np.random.randint(1001,100000),\n"%(temp)
+#                 inums = lnums[ 2 ] - 1
+#                 string[ inums ] = "\t12:\' -var buff 0.0 -var buffy 5.0 -var T %s -var swap_every 100 -var swap_atoms 267 -var rn %%s -var dump_every 100 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile traj.dump\'%%np.random.randint(1001,100000),\n"%(temp)
             #---
                 sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
                 os.system( 'python2 junk%s.py'%count )
