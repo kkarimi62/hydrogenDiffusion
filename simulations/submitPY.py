5bec1f71924d6f86baa90f627d29bd12bc795f0d
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-10 19:46:36 +0200

    update

--- simulations/submitPY.py
+++ simulations/submitPY.py
@@ -3,24 +3,24 @@ if __name__ == '__main__':
     import os
     import numpy as np
     #---
-#     lnums = [ 36, 108  ]
-#     string=open('simulations.py').readlines() #--- python script
-    lnums = [ 39, 116, 121 ]
-    string=open('simulations-ncbj-slurm.py').readlines() #--- python script
+    lnums = [ 36, 108  ]
+    string=open('simulations.py').readlines() #--- python script
+#    lnums = [ 39, 116, 121 ]
+#    string=open('simulations-ncbj-slurm.py').readlines() #--- python script
     #---
     Temps=			{
-#                 0:1000,
-#                 1:1200,
-#                 2:1400,
-#                 3:1600,
-#                 4:1800,
-                0:400,
-                1:500,
-                2:600,
-                3:700,
-                4:800,
-                5:900,
-                6:1000,
+                0:1000,
+                1:1200,
+                2:1400,
+                3:1600,
+                4:1800,
+#                 0:400,
+#                 1:500,
+#                 2:600,
+#                 3:700,
+#                 4:800,
+#                 5:900,
+#                 6:1000,
             }
     #---
     count = 0
@@ -28,8 +28,8 @@ if __name__ == '__main__':
                 temp = Temps[keys_t]
             #---	densities
                 inums = lnums[ 0 ] - 1
-#                string[ inums ] = "\t5:\'pureHydrogenDiffusion_md_MultipleTemps/temp%s\',\n"%(keys_t) #--- change job name
-                string[ inums ] = "\t7:\'biCrystalMultipleTemp2nd/temp%s\',\n"%(keys_t) #--- change job name
+                string[ inums ] = "\t5:\'pureHydrogenDiffusion_md_MultipleTemps/temp%s\',\n"%(keys_t) #--- change job name
+#                string[ inums ] = "\t7:\'biCrystalMultipleTemp2nd/temp%s\',\n"%(keys_t) #--- change job name
             #---
                 inums = lnums[ 1 ] - 1
 #                string[ inums ] = "\t\'p3\':\' data_minimized.txt init_xyz.conf %%s %s\'%%(os.getcwd()+\'/lmpScripts\'),\n"%temp
@@ -38,8 +38,8 @@ if __name__ == '__main__':
 
                 string[ inums ] = "\t72:\' -var seed %%s -var buff 0.0 -var buffy 5.0 -var Tinit 0.1 -var T %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData equilibrated.dat\'%%np.random.randint(1001,9999),\n"%(temp)
 
-                inums = lnums[ 2 ] - 1
-                string[ inums ] = "\t12:\' -var buff 0.0 -var buffy 5.0 -var T %s -var swap_every 100 -var swap_atoms 267 -var rn %%s -var dump_every 100 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile traj.dump\'%%np.random.randint(1001,100000),\n"%(temp)
+#                 inums = lnums[ 2 ] - 1
+#                 string[ inums ] = "\t12:\' -var buff 0.0 -var buffy 5.0 -var T %s -var swap_every 100 -var swap_atoms 267 -var rn %%s -var dump_every 100 -var ParseData 1 -var DataFile equilibrated.dat -var DumpFile traj.dump\'%%np.random.randint(1001,100000),\n"%(temp)
             #---
                 sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
                 os.system( 'python2 junk%s.py'%count )
