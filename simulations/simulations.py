cd52c6706e43b403eb3665e230640ea86eee15eb
kkarimi62 <kamran.karimi1362@gmail.com>
2023-06-14 14:09:41 +0200

    updates

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
-                5:'pureHydrogenDiffusionLowDensity2nd', #'pureHydrogenDiffusion_md',#'',
+                5:'pureHydrogenDiffusionLowDensitySingle', #'pureHydrogenDiffusion_md',#'',
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                }[5]
     sourcePath = os.getcwd() +\
@@ -117,7 +117,7 @@ if __name__ == '__main__':
                 'p3':' data_minimized.txt init_xyz.conf %s 1000.0'%(os.getcwd()+'/lmpScripts'),
                 'p4':' data_minimized.txt data_minimized.txt %s 1'%(os.getcwd()+'/lmpScripts'),
                 'p5':' ',
-                'p6':' %s data_minimized.txt data_minimized.txt 10'%(os.getcwd()+'/../../HeaDef/postprocess'),
+                'p6':' %s data_minimized.txt data_minimized.txt 1'%(os.getcwd()+'/../../HeaDef/postprocess'),
                 'p7':' sortieproc.0 0 Topo_ignore',
                  1.0:'-x DataFile=data_minimized.txt',
                  2.0:'-x DataFile=data_minimized.txt',
