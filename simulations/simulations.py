e0725aba52631746ec92400e88d76ec3cec327fc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-20 09:20:27 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
-                5:'hydrogenDiffusionExtendedDislocation4th',#'pureHydrogenDiffusionLowDensity4th',#'', #'hydrogenDiffusionLoop',#'hydrogenDiffusionExtendedDislocation',#'',
+                5:'hydrogenDiffusionExtendedDislocation4th', #'pureHydrogenDiffusionLowDensity4th',#'', #'hydrogenDiffusionLoop',#'hydrogenDiffusionExtendedDislocation',#'',
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                }[5]
     sourcePath = os.getcwd() +\
