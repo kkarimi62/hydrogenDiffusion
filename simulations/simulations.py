c4753ab4b41ae73bd13e3c59e686b0d74f2c4bca
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-22 14:18:13 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
-                5:'pureHydrogenDiffusionLowDensity5th',#'hydrogenDiffusionExtendedDislocation4th', #'',#'', #'hydrogenDiffusionLoop',#'hydrogenDiffusionExtendedDislocation',#'',
+                5:'pureHydrogenDiffusionLowDensity6th',#'hydrogenDiffusionExtendedDislocation4th', #'',#'', #'hydrogenDiffusionLoop',#'hydrogenDiffusionExtendedDislocation',#'',
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                }[5]
     sourcePath = os.getcwd() +\
@@ -113,7 +113,7 @@ if __name__ == '__main__':
                 11:' ',
                 'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
                 'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
-                'p2':' %s 3.52 30.0 14.0 10.0  data_minimized.txt 4 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
+                'p2':' %s 3.52 30.0 10.0 10.0  data_minimized.txt 0 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p21':' %s 3.52 52.0 18.0 26.0 data_init.txt 2 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p22':' %s 3.52 30.0 14.0 13.0  data_minimized.txt 6 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p3':' data_minimized.txt init_xyz.conf %s 1000.0'%(os.getcwd()+'/lmpScripts'),
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         #---
         for script,indx in zip(Pipeline,range(100)):
 #			os.system( 'cp %s/%s %s/lmpScript%s.txt' %( SCRPT_DIR, script, writPath, indx) ) #--- lammps script: periodic x, pxx, vy, load
-            os.system( 'ln -s %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+            os.system( 'cp -s %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
         if sourceFiles: 
             for sf in sourceFiles:
                 os.system( 'cp %s/Run%s/%s %s' %(sourcePath, irun, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
