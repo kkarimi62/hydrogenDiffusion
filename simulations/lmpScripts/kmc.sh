e889ad7094c5265026b90f40e1520b75f5f73559
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-22 17:53:24 +0200

    update

--- simulations/lmpScripts/kmc.sh
+++ simulations/lmpScripts/kmc.sh
@@ -23,7 +23,10 @@ setenv NBRE_KMC_STEPS                100 #0       # The max number of KMC steps 
 setenv TEMPERATURE                   ${temp} #500.0    # The simulated temperature in kelvin
 
 setenv NUMBER_ATOMS                   ${natom} #864     # The total number of atoms 
-setenv SIMULATION_BOX                ${lx} ${ly} ${lz} #21.47   # The size of the simulation box (x, y and z)
+#setenv SIMULATION_BOX                ${lx} #21.47   # The size of the simulation box (x, y and z)
+setenv X_BOX                         ${lx}
+setenv Y_BOX                         ${ly}
+setenv Z_BOX                         ${lz}
 setenv NSPECIES                       2 #3 #5     # The number of different atom types (default: 2)
 setenv ATOMIC_SYMBOLS					'Al:inactive H:active' #'Ni Co Cr Fe Mn' #"Ni Co Cr" #'Ni Co Cr Fe Mn' #"Ni Co Cr"
 #setenv NTRAVAILLEUR                     3     # The number of cores associated with forces calculations per ARTnouveau search (default:1)
