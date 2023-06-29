25789bef306924ae9516644c5f57ed195612e227
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-06-29 08:28:54 +0200

    update

--- postproc/postproc.py
+++ postproc/postproc.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     jobname  = {
                 '3':'NiNatom16KTemp1400K', 
                 '4':'CantorNatom16KTemp1400KEnsemble8', 
-                '5':'hydrogenDiffusionExtendedDislocation5th', 
+                '5':'pureHydrogenDiffusionLowDensity12th', 
                 }['5']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
