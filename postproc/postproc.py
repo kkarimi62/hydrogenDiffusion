46bd6a4e8791b36b55cc07300386844c59c0affc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-11 16:05:12 +0200

    update

--- postproc/postproc.py
+++ postproc/postproc.py
@@ -28,13 +28,13 @@ if __name__ == '__main__':
     jobname  = {
                 '3':'NiNatom16KTemp1400K', 
                 '4':'CantorNatom16KTemp1400KEnsemble8', 
-                '5':'singleHydrogenDiffusionLong', 
+                '5':'multiHydrogenDiffusionLong',#'singleHydrogenDiffusionLong', 
                 }['5']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '3':'/../simulations/NiNatom16KTemp1400K',
                                 '4':'/../simulations/CantorNatom16KTemp1000KEnsemble8',
-                                '5':'/../simulations/singleHydrogenDiffusionLong',
+                                '5':'/../simulations/multiHydrogenDiffusionLong',
                             }['5'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
