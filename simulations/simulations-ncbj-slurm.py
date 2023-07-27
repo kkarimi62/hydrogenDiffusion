fe1c53864355d02d594050aafc7ead2a102d6fc3
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-07-27 08:35:55 +0200

    update

--- simulations/simulations-ncbj-slurm.py
+++ simulations/simulations-ncbj-slurm.py
@@ -3,8 +3,8 @@ def makeOAR( EXEC_DIR, node, core, time ):
     print >> someFile, '#!/bin/bash\n'
     print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
     print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
-    print >> someFile, 'source ~/Project/opt/anaconda3/etc/profile.d/conda.sh\nconda activate deepmd3rd\nexport OMP_NUM_THREADS=%s'%(nThreads*nNode) #--- deep potential stuff
-#	print >> someFile, 'source /mnt/opt/spack-0.17/share/spack/setup-env.sh\nspack load openmpi@4.0.5 %gcc@9.3.0\nspack load openblas@0.3.18%gcc@9.3.0\nspack load python@3.8.12%gcc@8.3.0\n\n',
+#    print >> someFile, 'source ~/Project/opt/anaconda3/etc/profile.d/conda.sh\nconda activate deepmd3rd\nexport OMP_NUM_THREADS=%s'%(nThreads*nNode) #--- deep potential stuff
+    print >> someFile, 'source /mnt/opt/spack-0.17/share/spack/setup-env.sh\nspack load openmpi@4.0.5 %gcc@9.3.0\nspack load openblas@0.3.18%gcc@9.3.0\nspack load python@3.8.12%gcc@8.3.0\n\n',
 #	print >> someFile, 'export LD_LIBRARY_PATH=/mnt/opt/tools/cc7/lapack/3.5.0-x86_64-gcc46/lib:${LD_LIBRARY_PATH}\n'
 
     #--- run python script 
