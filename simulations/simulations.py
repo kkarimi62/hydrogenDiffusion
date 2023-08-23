11b1661aebcf3253a3ad509062f6d3bd0abb86d4
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-23 15:36:24 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     import os
     import numpy as np
 
-    nruns	 = range(8)
+    nruns	 = range(1) #8)
     #
     nThreads = 16  #8
     nNode	 = 1
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
-                5:'multiHydrogenDislocated/rho/rho3', #'multiHydrogenDiffusionLong/rho/rho0',
+                5:'test',#'multiHydrogenDislocated/rho/rho3', #'multiHydrogenDiffusionLong/rho/rho0',
                 6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
                }[5]
     sourcePath = os.getcwd() +\
@@ -116,7 +116,7 @@ if __name__ == '__main__':
                 11:' ',
                 'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
                 'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
-                'p2':' %s 3.54 17.0 9.0 8.0  data_minimized.txt 4 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
+                'p2':' %s 3.54 17.0 9.0 4.0  data_minimized.txt 4 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p21':' %s 3.54 52.0 18.0 26.0 data_init.txt 2 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p23':' %s 3.54 30.0 14.0 13.0 data_minimized.txt 6 5.0 2 1.0 0.0'%(os.getcwd()+'/lmpScripts'),
                 'p3':' data_minimized.txt init_xyz.conf %s 1000.0'%(os.getcwd()+'/lmpScripts'),
