b21ee2f52135b6f8af4bf9593db61d058a351ec5
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-23 08:08:05 +0200

    update

--- postproc/submitPY.py
+++ postproc/submitPY.py
@@ -1,14 +1,14 @@
-if __name__ == '__main__':
+    if __name__ == '__main__':
     import sys
     import os
     import numpy as np
     #---
-#    lnums = [ 30, 34 ]
-	lnums = [ 31, 37 ]
-#    string=open('postproc_ncbj_slurm.py').readlines() #--- python script
-	string=open('postproc.py').readlines() #--- python script
+    #    lnums = [ 30, 34 ]
+    lnums = [ 31, 37 ]
+    #    string=open('postproc_ncbj_slurm.py').readlines() #--- python script
+    string=open('postproc.py').readlines() #--- python script
     #---
-#    PHI  = dict(zip(range(6),np.linspace(1000,2000,6)))
+    #    PHI  = dict(zip(range(6),np.linspace(1000,2000,6)))
     PHI=			{
                 0:1,
                 1:2,
@@ -16,16 +16,16 @@ if __name__ == '__main__':
                 3:8,
             }
 
-#	PHI  = dict(zip(range(11),np.arange(1000,1440,40)))
-#		{ 
-#             '0':'FeNi',
-#            '1':'CoNiFe',
-#           '2':'CoNiCrFe',
-#           '3' :'CoCrFeMn',
-#            '4':'CoNiCrFeMn',
-#            '5':'Co5Cr2Fe40Mn27Ni26'
-#			'6':'cuzr',
-#		}
+    #	PHI  = dict(zip(range(11),np.arange(1000,1440,40)))
+    #		{ 
+    #             '0':'FeNi',
+    #            '1':'CoNiFe',
+    #           '2':'CoNiCrFe',
+    #           '3' :'CoCrFeMn',
+    #            '4':'CoNiCrFeMn',
+    #            '5':'Co5Cr2Fe40Mn27Ni26'
+    #			'6':'cuzr',
+    #		}
 
     nphi = len(PHI)
     #---
@@ -33,13 +33,13 @@ if __name__ == '__main__':
     for key in PHI:
             #---	
                 inums = lnums[ 0 ] - 1
-#				string[ inums ] = "\t\'4\':\'CantorNatom16KTemp%sKEnsemble8\',\n" % (int(PHI[key])) #--- change job name
-#				string[ inums ] = "\t\'1\':\'NiCoCrNatom1KTemp%sK\',\n" % (int(PHI[key])) #--- change job name
+    #				string[ inums ] = "\t\'4\':\'CantorNatom16KTemp%sKEnsemble8\',\n" % (int(PHI[key])) #--- change job name
+    #				string[ inums ] = "\t\'1\':\'NiCoCrNatom1KTemp%sK\',\n" % (int(PHI[key])) #--- change job name
                 string[ inums ] = "\t\'5\':\'multiHydrogenDislocated/rho/rho%s\',\n" % (key) #--- change job name
         #---	densities
                 inums = lnums[ 1 ] - 1
-#				string[ inums ] = "\t\'4\':\'/../simulations/CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[key]))
-#				string[ inums ] = "\t\'1\':\'/../simulations/NiCoCrNatom1KTemp%sK\',\n"%(int(PHI[key]))
+    #				string[ inums ] = "\t\'4\':\'/../simulations/CantorNatom16KTemp%sKEnsemble8\',\n"%(int(PHI[key]))
+    #				string[ inums ] = "\t\'1\':\'/../simulations/NiCoCrNatom1KTemp%sK\',\n"%(int(PHI[key]))
                 string[ inums ] = "\t\'5\':\'/../simulations/multiHydrogenDislocated/rho/rho%s\',\n"%(key)
         #
                 sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
