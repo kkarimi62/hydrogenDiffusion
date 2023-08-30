852ba49e7d6f262807b14564b31f59d3a391335f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-30 15:57:12 +0200

    update

--- simulations/lmpScripts/buffer.mod
+++ simulations/lmpScripts/buffer.mod
@@ -11,25 +11,48 @@ variable y0	equal ${yylo}+${buffy}
 variable yyhi	equal yhi
 variable y1		equal ${yyhi}-${buffy}
 #
+variable zzlo	equal zlo
+variable z0		equal ${zzlo}+${buffz}
+#
+variable zzhi	equal zhi 
+variable z1		equal ${zzhi}-${buffz}
+#
+region top    block INF INF INF INF ${z1} INF
+region bottom block INF INF INF INF INF ${z0}
+#
 region up block INF INF ${y1} INF INF INF
 region down block INF INF INF ${y0} INF INF
+#
 region right block ${x1} INF INF INF INF INF
 region left block INF ${x0} INF INF INF INF
+
+group topp region top
+group bottomm region bottom
+#
 group upp region up
 group downn region down
+#
 group lg region left
 group rg region right
 #
 #--- fix walls
 fix 1 upp setforce 0.0 0.0 0.0
 fix 2 downn setforce 0.0 0.0 0.0
+#
 fix 11 lg setforce 0.0 0.0 0.0
 fix 22 rg setforce 0.0 0.0 0.0
+#
+fix 111 topp setforce 0.0 0.0 0.0
+fix 222 bottomm setforce 0.0 0.0 0.0
+#
+
+velocity topp set 0 0 0
+velocity bottomm set 0 0 0
 velocity upp set 0 0 0
 velocity downn set 0 0 0
 velocity lg set 0 0 0
 velocity rg set 0 0 0
 
-group boundary union upp downn lg rg
+group boundary union upp downn lg rg topp bottomm
 group bulk subtract all boundary
 
--- simulations/simulations.py
+++ simulations/simulations.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
                 52:' -var buff 0.0 -var buffy 0.0 -var pyy 100000.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
                 7:' -var buff 0.0 -var T 1500.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
                 71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
-                72:' -var buff 0.0 -var buffy 5.0 -var Tinit 700.0 -var T 700.0 -var seed %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated.dat'%np.random.randint(1001,9999),
+                72:' -var buff 0.0 -var buffy 0.0 -var buffz 3.0 -var Tinit 700.0 -var T 700.0 -var seed %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated.dat'%np.random.randint(1001,9999),
                 8:' -var buff 0.0 -var T 300.0 -var sigm 1.0 -var sigmdt 0.0001 -var ndump 100 -var ParseData 1 -var DataFile Equilibrated_0.dat -var DumpFile dumpSheared.xyz',
                 9:' -var natoms 1000 -var cutoff 3.52 -var ParseData 1',
                 10:' -var ParseData 1 -var DataFile swapped_600.dat',
