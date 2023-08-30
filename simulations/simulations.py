a15b30817f9ac11da605f8837475cb2e4170405f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-30 14:36:26 +0200

    update

--- simulations/simulations.py
+++ simulations/simulations.py
@@ -27,15 +27,15 @@ if __name__ == '__main__':
 
     nruns	 = range(8)
     #
-    nThreads = 16  #8
+    nThreads = 2 #16  #8
     nNode	 = 1
     #
     jobname  = {
                 3:'hydrogenDiffusionInAlMultipleTemp/Temp1000K', 
                 4:'mitStuff2nd', 
                 5:'hydrogenDiffusionLoopMultipleHydrogen/rho/rho0', #'multiHydrogenDislocated/rho/rho3', #'multiHydrogenDiffusionLong/rho/rho0',
-                6:'hydrogenDiffusionInAlBigMultipleTemps10H/temp0', #'hydrogenFree',
-               }[5]
+                6:'hydrogenDiffusionLoopMultipleHydrogenMd/temp0', #'hydrogenFree',
+               }[6]
     sourcePath = os.getcwd() +\
                 {	
                     0:'/junk',
@@ -109,7 +109,7 @@ if __name__ == '__main__':
                 52:' -var buff 0.0 -var buffy 0.0 -var pyy 100000.0 -var nevery 1000 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpMin.xyz -var WriteData data_minimized.txt', 
                 7:' -var buff 0.0 -var T 1500.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
                 71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
-                72:' -var buff 0.0 -var buffy 0.0 -var Tinit 1000 -var T 1000.0 -var seed %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated.dat'%np.random.randint(1001,9999),
+                72:' -var buff 0.0 -var buffy 0.0 -var Tinit 1400 -var T 1400.0 -var seed %s -var nevery 100 -var ParseData 1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated.dat'%np.random.randint(1001,9999),
                 8:' -var buff 0.0 -var T 300.0 -var sigm 1.0 -var sigmdt 0.0001 -var ndump 100 -var ParseData 1 -var DataFile Equilibrated_0.dat -var DumpFile dumpSheared.xyz',
                 9:' -var natoms 1000 -var cutoff 3.52 -var ParseData 1',
                 10:' -var ParseData 1 -var DataFile swapped_600.dat',
@@ -143,7 +143,8 @@ if __name__ == '__main__':
                 81:[5,'p6',51,'p3','p5',1.0], #--- minimize,add H, minimize, kart input, kart.sh to bash shell ,invoke kart
                 91:['p2',51,'p6',51,'p3','p5',1.0], #--- dislocate, minimize,add H, minimize, kart input, kart.sh to bash shell ,invoke kart
                 100:['p23',51, 52, 'p6',51,'p3','p5',1.0], #--- loop, minimize (zero p!), pressurize, add H, minimize (zero p!), kart input, kart.sh to bash shell ,invoke kart
-              }[ 100 ]
+                101:['p23',51, 52, 'p6',51,72], #--- loop, minimize (zero p!), pressurize, add H, minimize (zero p!), kart input, kart.sh to bash shell ,invoke kart
+              }[ 101 ]
     Pipeline = list(map(lambda x:LmpScript[x],indices))
 #	Variables = list(map(lambda x:Variable[x], indices))
     EXEC = list(map(lambda x:np.array(['lmp','py','kmc'])[[ type(x) == type(0), type(x) == type(''), type(x) == type(1.0) ]][0], indices))	
